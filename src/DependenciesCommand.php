1be2cc49027339833c72e61cd48536d27516661d
patrickkusebauch <patrick.kusebauch@gmail.com>
2022-06-03 23:14:05 +0200

    Fixed DependenciesCommand.php

--- src/DependenciesCommand.php
+++ src/DependenciesCommand.php
@@ -2,132 +2,149 @@
 
 declare(strict_types=1);
 
-//namespace DanceEngineer\DeptracAwesome;
-//
-//use Exception;
-//use Qossmic\Deptrac\Analyser\AstMapExtractor;
-//use Qossmic\Deptrac\Analyser\LayerForTokenAnalyser;
-//use Qossmic\Deptrac\Console\Symfony\Style;
-//use Qossmic\Deptrac\Dependency\DependencyResolver;
-//use Qossmic\Deptrac\Result\Violation;
-//use Symfony\Component\Console\Command\Command;
-//use Symfony\Component\Console\Input\InputArgument;
-//use Symfony\Component\Console\Input\InputInterface;
-//use Symfony\Component\Console\Output\OutputInterface;
-//use Symfony\Component\Console\Style\SymfonyStyle;
-//
-//final class DependenciesCommand extends Command
-//{
-//    public static $defaultName = 'dependencies';
-//
-//    public static $defaultDescription = 'List layer dependencies';
-//
-//    private AstMapExtractor $astMapExtractor;
-//
-//    private DependencyResolver $dependencyResolver;
-//
-//    private LayerForTokenAnalyser $layerForTokenAnalyser;
-//
-//    public function __construct(
-//        AstMapExtractor $astMapExtractor,
-//        DependencyResolver $dependencyResolver,
-//        LayerForTokenAnalyser $layerForTokenAnalyser
-//    ) {
-//        parent::__construct();
-//        $this->astMapExtractor = $astMapExtractor;
-//        $this->dependencyResolver = $dependencyResolver;
-//        $this->layerForTokenAnalyser = $layerForTokenAnalyser;
-//    }
-//
-//    protected function configure(): void
-//    {
-//        parent::configure();
-//
-//        /** @throws void */
-//        $this->addArgument('layer', InputArgument::REQUIRED, 'Layer to debug');
-//        /** @throws void */
-//        $this->addArgument(
-//            'targetLayer',
-//            InputArgument::OPTIONAL,
-//            'Target layer to filter dependencies to only one layer'
-//        );
-//    }
-//
-//    protected function execute(InputInterface $input, OutputInterface $output): int
-//    {
-//        $outputStyle = new Style(new SymfonyStyle($input, $output));
-//        try {
-//            $target = $input->getArgument('targetLayer');
-//            $result = $this->getDependencies(
-//                (string) $input->getArgument('layer'),
-//                $target === null ? null : (string) $target
-//            );
-//
-//            foreach ($result as $item) {
-//                $outputStyle->table(
-//                    ['Dependencies'],
-//                    array_map(fn (Violation $violation): array => $this->violationRow($violation), $item)
-//                );
-//            }
-//            return self::SUCCESS;
-//        } catch (Exception $exception) {
-//            $outputStyle->error($exception->getMessage());
-//            return self::FAILURE;
-//        }
-//    }
-//
-//    /**
-//     * @return array<string>
-//     */
-//    private function violationRow(Violation $rule): array
-//    {
-//        $dependency = $rule->getDependency();
-//
-//        $message = sprintf(
-//            '<info>%s</info> depends on <info>%s</info> (%s)',
-//            $dependency->getDepender()
-//                ->toString(),
-//            $dependency->getDependent()
-//                ->toString(),
-//            $rule->getDependentLayer()
-//        );
-//
-//        $fileOccurrence = $rule->getDependency()
-//            ->getFileOccurrence();
-//        $message .= sprintf("\n%s:%d", $fileOccurrence->getFilepath(), $fileOccurrence->getLine());
-//
-//        return [$message];
-//    }
-//
-//    /**
-//     * @return array<string, array<Violation>>
-//     */
-//    private function getDependencies(string $layer, ?string $targetLayer): array
-//    {
-//        $result = [];
-//        $astMap = $this->astMapExtractor->extract();
-//        $dependencies = $this->dependencyResolver->resolve($astMap);
-//
-//        foreach ($dependencies->getDependenciesAndInheritDependencies() as $dependency) {
-//            $dependerLayerNames = $this->layerForTokenAnalyser->findLayerForToken(
-//                $dependency->getDepender()
-//                    ->toString(),
-//                'class-like'
-//            );
-//            if (in_array($layer, $dependerLayerNames, true)) {
-//                $dependentLayerNames = $this->layerForTokenAnalyser->findLayerForToken(
-//                    $dependency->getDependent()
-//                        ->toString(),
-//                    'class-like'
-//                );
-//                foreach ($dependentLayerNames as $dependentLayerName) {
-//                    if ($layer === $dependentLayerName || ($targetLayer !== null && $targetLayer !== $dependentLayerName)) {
-//                        continue;
-//                    }
-//                    $result[$dependentLayerName][] = new Violation($dependency, $layer, $dependentLayerName);
-//                }
-//            }
-//        }
-//        return $result;
-//    }
-//}
+namespace DanceEngineer\DeptracAwesome;
+
+use Exception;
+use Qossmic\Deptrac\Analyser\AstMapExtractor;
+use Qossmic\Deptrac\Console\Symfony\Style;
+use Qossmic\Deptrac\Dependency\DependencyResolver;
+use Qossmic\Deptrac\Dependency\TokenResolver;
+use Qossmic\Deptrac\Layer\LayerResolver;
+use Qossmic\Deptrac\Result\Uncovered;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Stopwatch\Stopwatch;
+
+final class DependenciesCommand extends Command
+{
+    public static $defaultName = 'dependencies';
+
+    public static $defaultDescription = 'List layer dependencies';
+
+    private AstMapExtractor $astMapExtractor;
+
+    private DependencyResolver $dependencyResolver;
+
+    private LayerResolver $layerResolver;
+
+    private TokenResolver $tokenResolver;
+
+    public function __construct(
+        AstMapExtractor $astMapExtractor,
+        DependencyResolver $dependencyResolver,
+        LayerResolver $layerResolver,
+        TokenResolver $tokenResolver,
+    ) {
+        parent::__construct();
+        $this->astMapExtractor = $astMapExtractor;
+        $this->dependencyResolver = $dependencyResolver;
+        $this->layerResolver = $layerResolver;
+        $this->tokenResolver = $tokenResolver;
+    }
+
+    protected function configure(): void
+    {
+        parent::configure();
+
+        /** @throws void */
+        $this->addArgument('layer', InputArgument::REQUIRED, 'Layer to debug');
+        /** @throws void */
+        $this->addArgument(
+            'targetLayer',
+            InputArgument::OPTIONAL,
+            'Target layer to filter dependencies to only one layer'
+        );
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $stopwatch = new Stopwatch();
+        $stopwatch->start('command');
+
+        $outputStyle = new Style(new SymfonyStyle($input, $output));
+        $status      = self::SUCCESS;
+        try {
+            $target = $input->getArgument('targetLayer');
+
+            $dependencies = $this->getDependencies(
+                (string) $input->getArgument('layer'),
+                $target === null ? null : (string) $target
+            );
+
+            foreach ($dependencies as $layer => $violations) {
+                $outputStyle->table(
+                    [$layer],
+                    array_map(fn (Uncovered $violation): array => $this->formatRow($violation), $violations)
+                );
+            }
+        } catch (Exception $exception) {
+            $outputStyle->error($exception->getMessage());
+            $status = self::FAILURE;
+        }
+
+        $output->writeln(
+            sprintf(
+                'The command finished in %1.2f seconds',
+                $stopwatch->stop('command')
+                    ->getDuration() / 1000.0
+            )
+        );
+
+        return $status;
+    }
+
+    /**
+     * @return array<string>
+     */
+    private function formatRow(Uncovered $rule): array
+    {
+        $dependency = $rule->getDependency();
+
+        $message = sprintf(
+            '<info>%s</info> depends on <info>%s</info> (%s)',
+            $dependency->getDepender()
+                ->toString(),
+            $dependency->getDependent()
+                ->toString(),
+            $rule->getLayer()
+        );
+
+        $fileOccurrence = $dependency->getFileOccurrence();
+        $message .= sprintf("\n%s:%d", $fileOccurrence->getFilepath(), $fileOccurrence->getLine());
+
+        return [$message];
+    }
+
+    /**
+     * @return array<string, array<Uncovered>>
+     */
+    private function getDependencies(string $layer, ?string $targetLayer): array
+    {
+
+        $result = [];
+        $astMap = $this->astMapExtractor->extract();
+        $dependencies = $this->dependencyResolver->resolve($astMap);
+        foreach ($dependencies->getDependenciesAndInheritDependencies() as $dependency) {
+            $dependerLayerNames = $this->layerResolver->getLayersForReference(
+                $this->tokenResolver->resolve($dependency->getDepender(), $astMap),
+                $astMap
+            );
+            if (in_array($layer, $dependerLayerNames, true)) {
+                $dependentLayerNames = $this->layerResolver->getLayersForReference(
+                    $this->tokenResolver->resolve($dependency->getDependent(), $astMap),
+                    $astMap
+                );
+                foreach ($dependentLayerNames as $dependentLayerName) {
+                    if ($layer === $dependentLayerName || ($targetLayer !== null && $targetLayer !== $dependentLayerName)) {
+                        continue;
+                    }
+                    $result[$dependentLayerName][] = new Uncovered($dependency, $dependentLayerName);
+                }
+            }
+        }
+        return $result;
+    }
+}
